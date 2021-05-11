fc3efc959f4ff3a1e1e4665d84fee2234e58df7f
root <antoinepoidevin@hotmail.fr>
2021-05-11 15:29:01 +0200

    Auto-Sauvegarde-Server 2021-05-11-15:29:01

--- .gitpush.sh
+++ .gitpush.sh
@@ -1,7 +1,4 @@
 
-DATE=$(date)
-
-git add .
-git commit -a -m "Auto-Sauvegarde-Server `date +%F-%T`"
-git push 
-
+git -C /home/poidevin/minecraft/minecraft_server/ add .
+git -C /home/poidevin/minecraft/minecraft_server/ commit -m "Auto-Sauvegarde-Server `date +%F-%T`"
+git -C /home/poidevin/minecraft/minecraft_server/ push 
